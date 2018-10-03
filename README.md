b577d89881e20433a084821f1cd2a61eb04024fb
Serban Iordache <serban.iordache@scoop-software.de>
2018-10-03 03:24:19 +0200

    initial release

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-Repository created by BootHub
+Example [Text-IO](https://github.com/beryx/text-io) application that registers handlers for the up and down arrow keys in order to
+display available choices.
+
+See [issue #17]()https://github.com/beryx/text-io/issues/17) for details.
+
+
+**Getting started**
+
+```
+git clone https://github.com/beryx-gist/text-io-swing-handler.git
+cd  text-io-swing-handler
+```
+
+- on Unix-like systems:
+```
+./gradlew installDist
+cd build/install/text-io-swing-handler/bin
+./text-io-swing-handler
+```
+
+
+- on Windows systems:
+```
+gradlew installDist
+cd build\install\text-io-swing-handler\bin
+text-io-swing-handler
+```
--- src/main/java/org/beryx/swing/handler/SwingHandler.java
+++ src/main/java/org/beryx/swing/handler/SwingHandler.java
@@ -15,8 +15,87 @@
  */
 package org.beryx.swing.handler;
 
+import org.apache.commons.lang3.StringUtils;
+import org.beryx.textio.ReadHandlerData;
+import org.beryx.textio.ReadInterruptionStrategy;
+import org.beryx.textio.TextIO;
+import org.beryx.textio.swing.SwingTextTerminal;
+
+import java.time.Month;
+import java.util.stream.Stream;
+
 public class SwingHandler {
-	public static void main(String[] args) {
-		System.out.println("Hello from SwingHandler!");
-	}
+    private static final String KEY_STROKE_UP = "pressed UP";
+    private static final String KEY_STROKE_DOWN = "pressed DOWN";
+
+    private final SwingTextTerminal terminal;
+
+    private String originalInput = "";
+    private int choiceIndex = -1;
+    private String[] choices = {};
+
+
+    public SwingHandler(SwingTextTerminal terminal) {
+        this.terminal = terminal;
+        terminal.registerHandler(KEY_STROKE_UP, t -> {
+            if(choiceIndex < 0) {
+                originalInput = terminal.getPartialInput();
+            }
+            if(choiceIndex < choices.length - 1) {
+                choiceIndex++;
+                t.replaceInput(choices[choiceIndex], false);
+            }
+            return new ReadHandlerData(ReadInterruptionStrategy.Action.CONTINUE);
+        });
+
+        terminal.registerHandler(KEY_STROKE_DOWN, t -> {
+            if(choiceIndex >= 0) {
+                choiceIndex--;
+                String text = (choiceIndex < 0) ? originalInput : choices[choiceIndex];
+                t.replaceInput(text, false);
+            }
+            return new ReadHandlerData(ReadInterruptionStrategy.Action.CONTINUE);
+        });
+    }
+
+    public void setChoices(String... choices) {
+        this.originalInput = "";
+        this.choiceIndex = -1;
+        this.choices = choices;
+    }
+
+    public static void main(String[] args) {
+        SwingTextTerminal terminal = new SwingTextTerminal();
+        terminal.init();
+        TextIO textIO = new TextIO(terminal);
+        SwingHandler handler = new SwingHandler(terminal);
+
+        terminal.println("-------------------------------------------------------------------------");
+        terminal.println("|   You can use the up and down arrow keys to scroll through choices.   |");
+        terminal.println("-------------------------------------------------------------------------\n");
+
+        handler.setChoices("albert", "alice", "ava", "betty", "cathy");
+        String player = textIO.newStringInputReader().read("Player name");
+
+        // No choices here
+        handler.setChoices();
+        int points = textIO.newIntInputReader().read("Total points");
+
+        String[] monthNames = Stream.of(Month.values())
+                .map(Month::name)
+                .map(String::toLowerCase)
+                .map(StringUtils::capitalize)
+                .toArray(String[]::new);
+        handler.setChoices(monthNames);
+        String month = textIO.newStringInputReader()
+                .withInlinePossibleValues(monthNames)
+                .withIgnoreCase()
+                .withPromptAdjustments(false)
+                .read("Month: ");
+
+        terminal.printf("\nHello, %s! You earned %d points in %s.\n\n", player, points, month);
+
+        textIO.newStringInputReader().withMinLength(0).read("\nPress enter to terminate...");
+        textIO.dispose();
+    }
 }

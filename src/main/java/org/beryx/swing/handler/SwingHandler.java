2cdae171c657bcb43a96f0c23160cfda89785b50
Serban Iordache <iordache@hotmail.com>
2018-10-04 02:18:22 +0200

    change addTask method

--- src/main/java/org/beryx/swing/handler/SwingHandler.java
+++ src/main/java/org/beryx/swing/handler/SwingHandler.java
@@ -15,10 +15,7 @@
  */
 package org.beryx.swing.handler;
 
-import org.beryx.textio.ReadAbortedException;
-import org.beryx.textio.ReadHandlerData;
-import org.beryx.textio.ReadInterruptionStrategy;
-import org.beryx.textio.TextIO;
+import org.beryx.textio.*;
 import org.beryx.textio.swing.SwingTextTerminal;
 
 import java.util.ArrayList;
@@ -32,6 +29,7 @@ public class SwingHandler {
     private static final String KEY_STROKE_UP = "pressed UP";
     private static final String KEY_STROKE_DOWN = "pressed DOWN";
 
+    private final TextIO textIO;
     private final SwingTextTerminal terminal;
 
     private final String backKeyStroke;
@@ -65,8 +63,10 @@ public class SwingHandler {
 
     private final List<Runnable> operations = new ArrayList<>();
 
-    public SwingHandler(SwingTextTerminal terminal) {
-        this.terminal = terminal;
+    public SwingHandler() {
+        this.terminal = new SwingTextTerminal();
+        terminal.init();
+        this.textIO = new TextIO(terminal);
 
         this.backKeyStroke = terminal.getProperties().getString("custom.back.key", "ctrl U");
 
@@ -93,12 +93,16 @@ public class SwingHandler {
         terminal.registerHandler(backKeyStroke, t -> new ReadHandlerData(ABORT));
     }
 
+    public TextIO getTextIO() {
+        return textIO;
+    }
+
     public String getBackKeyStroke() {
         return backKeyStroke;
     }
 
-    private void addTask(TextIO textIO, String prompt,
-                         Supplier<String> defaultValueSupplier, Consumer<String> valueSetter, String... choices) {
+    private void addTask(String prompt, Supplier<String> defaultValueSupplier,
+                         Consumer<String> valueSetter, String... choices) {
         operations.add(() -> {
             setChoices(choices);
             valueSetter.accept(textIO.newStringInputReader()
@@ -136,10 +140,9 @@ public class SwingHandler {
     }
 
     public static void main(String[] args) {
-        SwingTextTerminal terminal = new SwingTextTerminal();
-        terminal.init();
-        TextIO textIO = new TextIO(terminal);
-        SwingHandler handler = new SwingHandler(terminal);
+        SwingHandler handler = new SwingHandler();
+        TextIO textIO = handler.getTextIO();
+        TextTerminal<?> terminal = textIO.getTextTerminal();
 
         terminal.println("----------------------------------------------------------------");
         terminal.println("|   Use the up and down arrow keys to scroll through choices.  |");
@@ -147,19 +150,19 @@ public class SwingHandler {
         terminal.println("----------------------------------------------------------------\n");
 
         Contact contact = new Contact();
-        handler.addTask(textIO, "First name", () -> contact.firstName, s -> contact.firstName = s,
+        handler.addTask("First name", () -> contact.firstName, s -> contact.firstName = s,
                 "albert", "alice", "ava", "betty", "cathy");
-        handler.addTask(textIO, "Last name", () -> contact.lastName, s -> contact.lastName = s,
+        handler.addTask("Last name", () -> contact.lastName, s -> contact.lastName = s,
                 "Adams", "Bush", "Clinton", "Eisenhower", "Ford");
-        handler.addTask(textIO, "Street address", () -> contact.streetAddress, s -> contact.streetAddress = s);
-        handler.addTask(textIO, "City", () -> contact.city, s -> contact.city = s,
+        handler.addTask("Street address", () -> contact.streetAddress, s -> contact.streetAddress = s);
+        handler.addTask("City", () -> contact.city, s -> contact.city = s,
                 "Los Angeles", "New York", "San Francisco", "Washington");
-        handler.addTask(textIO, "Zip code", () -> contact.zipCode, s -> contact.zipCode = s);
-        handler.addTask(textIO, "State", () -> contact.state, s -> contact.state = s,
+        handler.addTask("Zip code", () -> contact.zipCode, s -> contact.zipCode = s);
+        handler.addTask("State", () -> contact.state, s -> contact.state = s,
                 "Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware");
-        handler.addTask(textIO, "Country", () -> contact.country, s -> contact.country = s,
+        handler.addTask("Country", () -> contact.country, s -> contact.country = s,
                 "China", "France", "Germany", "Japan", "South Korea", "USA");
-        handler.addTask(textIO, "Phone number", () -> contact.phone, s -> contact.phone = s);
+        handler.addTask("Phone number", () -> contact.phone, s -> contact.phone = s);
 
         handler.execute();
 
